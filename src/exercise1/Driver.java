ea586bc465cd04690e94f5f2e4eea2d3dfc8a077
ChenLI0429 <chen.li@edu.sait.ca>
2025-06-16 13:37:06 -0600

    Initial commit

---
+++ src/exercise1/Driver.java
@@ -0,0 +1,43 @@
+package exercise1;
+//import exercise1.StudentAgeComparator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Driver {
+
+    public static void main(String[] args) {
+        
+        ArrayList<Student> studs = new ArrayList<>();
+        studs.add(new Student("Smith", 34));
+        studs.add(new Student("Johnson", 21));
+        studs.add(new Student("Williams", 67));
+        studs.add(new Student("Brown", 53));
+        studs.add(new Student("Jones", 48));
+        studs.add(new Student("Miller", 36));
+        studs.add(new Student("Davis", 44));
+        studs.add(new Student("Wilson", 52));
+        studs.add(new Student("Anderson", 34));
+        studs.add(new Student("Moore", 33));
+
+        System.out.println("--------Original list--------");
+        for (Student stud : studs) {
+            System.out.println(stud.toString());
+        }
+
+        // Sort using Comparable (by name)
+        Collections.sort(studs);
+        System.out.println("\n------Comparable: Order by Name ------");
+        for (Student stud : studs) {
+            System.out.println(stud.toString());
+        }
+
+        // Sort using Comparator (by age)
+        Collections.sort(studs, new StudentAgeComparator());
+        System.out.println("\n------ Comparator: Order by Age ------");
+        for (Student stud : studs) {
+            System.out.println(stud.toString());
+        }
+    }
+}
---
+++ src/exercise1/StudentAgeComparator.java
@@ -0,0 +1,14 @@
+package exercise1;
+import java.util.Comparator;
+
+public class StudentAgeComparator implements Comparator<Student> {
+    @Override
+    public int compare(Student s1, Student s2) {
+        //Sort by age
+        if (s1.getAge() != s2.getAge()) {
+            return Integer.compare(s1.getAge(), s2.getAge());
+        }
+        //If ages are equal, sort by name
+        return s1.getName().compareTo(s2.getName());
+    }
+}
